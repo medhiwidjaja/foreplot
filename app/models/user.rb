dbcd7f592ab4b3149b7cb4da5e1b0c4b46cf8e1d
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-06-10 21:37:06 +0700

    Pass user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true
+  validates_length_of :password, minimum: 8, on: :create
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i } 
+
 end
