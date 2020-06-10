543c586f38e08b97e4240b57db2bd518afae4c35
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-06-10 21:36:48 +0700

    User spec with new syntax

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,28 +2,40 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let(:darcy) { create(:darcy) }
+  let!(:darcy) { create(:darcy) }
+
+  subject { darcy }
 
   it { should respond_to(:name) } 
   it { should respond_to(:email) } 
 
-  it { should be_valid }
+  it { is_expected.to be_valid }
 
   describe "when name is not present" do
     before { darcy.name = " " }
-    it { should_not be_valid }
+    it { is_expected.not_to be_valid }
   end
 
   describe "when email is not present" do
     before { darcy.email = " " }
-    it { should_not be_valid }
+    it { is_expected.not_to be_valid }
   end
 
   describe "when email is a duplicate" do
     let(:another_darcy) { build(:darcy) }
     it "should be invalid" do
-      assert another_user.invalid?
+      expect(another_darcy).not_to be_valid
     end
+  end
+
+  describe "when password is too short" do
+    before { darcy.password = "12345" }
+    it { is_expected.not_to be_valid }
+  end
+
+  describe "when email is not properly formatted" do
+    before { darcy.email = "what is this?" }
+    it { is_expected.not_to be_valid }
   end
 
 end
