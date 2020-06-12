2527ebabe9cd3ddb43c38658fdbddc8974c0172d
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-06-12 11:02:38 +0700

    Article with sign up

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index]
+  
   # GET /articles
   # GET /articles.json
   def index
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -29,113 +29,138 @@ RSpec.describe ArticlesController, type: :controller do
   # Article. As you add validations to Article, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { title: 'My article', description: 'This is my first article', slug: 'my-article', private: false, active: true }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { title: '', active: true }
   }
+
+  let(:bingley) { create(:bingley) }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # ArticlesController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "returns a success response" do
-      Article.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_successful
+  context "without signed in user" do
+    describe "GET #index" do
+      it "returns a success response" do
+        Article.create! valid_attributes
+        get :index, params: {}, session: valid_session
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET #show" do
+      it "redirects to sign in page" do
+        article = Article.create! valid_attributes
+        get :show, params: {id: article.to_param}, session: valid_session
+        expect(response).to redirect_to( new_user_session_path )
+      end
     end
   end
 
-  describe "GET #show" do
-    it "returns a success response" do
-      article = Article.create! valid_attributes
-      get :show, params: {id: article.to_param}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
+  context "with signed in user" do
+    before(:each) {
+      sign_in bingley
+    }
 
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}, session: valid_session
-      expect(response).to be_successful
+    describe "GET #index" do
+      it "returns a success response" do
+        Article.create! valid_attributes
+        get :index, params: {}, session: valid_session
+        expect(response).to be_successful
+      end
     end
-  end
 
-  describe "GET #edit" do
-    it "returns a success response" do
-      article = Article.create! valid_attributes
-      get :edit, params: {id: article.to_param}, session: valid_session
-      expect(response).to be_successful
+    describe "GET #show" do
+      it "returns a success response" do
+        article = Article.create! valid_attributes
+        get :show, params: {id: article.to_param}, session: valid_session
+        expect(response).to be_successful
+      end
     end
-  end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Article" do
-        expect {
+    describe "GET #new" do
+      it "returns a success response" do
+        get :new, params: {}, session: valid_session
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET #edit" do
+      it "returns a success response" do
+        article = Article.create! valid_attributes
+        get :edit, params: {id: article.to_param}, session: valid_session
+        expect(response).to be_successful
+      end
+    end
+
+    describe "POST #create" do
+      context "with valid params" do
+        it "creates a new Article" do
+          expect {
+            post :create, params: {article: valid_attributes}, session: valid_session
+          }.to change(Article, :count).by(1)
+        end
+
+        it "redirects to the created article" do
           post :create, params: {article: valid_attributes}, session: valid_session
-        }.to change(Article, :count).by(1)
+          expect(response).to redirect_to(Article.last)
+        end
       end
 
-      it "redirects to the created article" do
-        post :create, params: {article: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(Article.last)
+      context "with invalid params" do
+        it "returns a success response (i.e. to display the 'new' template)" do
+          post :create, params: {article: invalid_attributes}, session: valid_session
+          expect(response).to be_successful
+        end
+      end
+    end
+
+    describe "PUT #update" do
+      context "with valid params" do
+        let(:new_attributes) {
+          { title: 'My inactive article', description: '', slug: 'my-article', private: false, active: false }
+        }
+
+        it "updates the requested article" do
+          article = Article.create! valid_attributes
+          put :update, params: {id: article.to_param, article: new_attributes}, session: valid_session
+          article.reload
+          expect(article.active).to eq(false) 
+        end
+
+        it "redirects to the article" do
+          article = Article.create! valid_attributes
+          put :update, params: {id: article.to_param, article: valid_attributes}, session: valid_session
+          expect(response).to redirect_to(article)
+        end
+      end
+
+      context "with invalid params" do
+        it "returns a success response (i.e. to display the 'edit' template)" do
+          article = Article.create! valid_attributes
+          put :update, params: {id: article.to_param, article: invalid_attributes}, session: valid_session
+          expect(response).to be_successful
+        end
       end
     end
 
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {article: invalid_attributes}, session: valid_session
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested article" do
+    describe "DELETE #destroy" do
+      it "destroys the requested article" do
         article = Article.create! valid_attributes
-        put :update, params: {id: article.to_param, article: new_attributes}, session: valid_session
-        article.reload
-        skip("Add assertions for updated state")
+        expect {
+          delete :destroy, params: {id: article.to_param}, session: valid_session
+        }.to change(Article, :count).by(-1)
       end
 
-      it "redirects to the article" do
+      it "redirects to the articles list" do
         article = Article.create! valid_attributes
-        put :update, params: {id: article.to_param, article: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(article)
-      end
-    end
-
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'edit' template)" do
-        article = Article.create! valid_attributes
-        put :update, params: {id: article.to_param, article: invalid_attributes}, session: valid_session
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested article" do
-      article = Article.create! valid_attributes
-      expect {
         delete :destroy, params: {id: article.to_param}, session: valid_session
-      }.to change(Article, :count).by(-1)
-    end
-
-    it "redirects to the articles list" do
-      article = Article.create! valid_attributes
-      delete :destroy, params: {id: article.to_param}, session: valid_session
-      expect(response).to redirect_to(articles_url)
+        expect(response).to redirect_to(articles_url)
+      end
     end
   end
-
 end
