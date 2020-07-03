318ed99fc8393d196e297eb1d72fb413a5e24027
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-07-03 14:44:28 +0700

    Configure app to load files from /lib folders

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Desight
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    config.autoload_once_paths += %W[#{config.root}/lib]
   end
 end
