5dd850edf66bdce3c4d700973f8bb658791fe9a0
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-08-05 08:54:19 +0700

    Direct comparison specs

--- spec/factories/comparisons.rb
+++ spec/factories/comparisons.rb
@@ -1,16 +1,26 @@
 FactoryBot.define do
   factory :comparison do
-    comparable_id { 1 }
-    comparable_type { "MyString" }
     title { "MyString" }
     notes { "MyString" }
-    score { "9.99" }
-    score_n { "9.99" }
-    comparison_method { "MyString" }
-    value { "9.99" }
-    unit { "MyString" }
-    rank_no { 1 }
-    rank_method { "MyString" }
-    consistency { "9.99" }
+    score { 0.99 }
+    score_n { 0.19 }
+    comparable  factory: :criterion
+
+    trait :direct_comparison do
+      comparison_method { "DirectComparison" }
+      value { 9.99 }
+      unit { "unit" }
+    end
+
+    trait :magiq_comparison do
+      comparison_method { "MagiqComparison" }
+      rank_no { 1 }
+      rank_method { "ROC" }
+    end
+
+    trait :pairwise_comparison do
+      comparison_method { "PairwiseComparison" }
+      consistency { 0.2 }
+    end
   end
 end
