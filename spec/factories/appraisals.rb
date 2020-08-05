30ad03c5b9e19f6d5645a95b9a9903f546d209ce
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-08-05 12:22:31 +0700

    Add default appraisal_method in test

--- spec/factories/appraisals.rb
+++ spec/factories/appraisals.rb
@@ -4,9 +4,10 @@ FactoryBot.define do
     member 
     is_valid      { true }
     is_complete   { true }
-
+    appraisal_method  { "DirectComparison" }
+    
     trait :with_direct_comparisons do
-      appraisal_method  { "direct" }
+      appraisal_method  { "DirectComparison" }
       after :create do |appraisal|
         3.times do |i|
           appraisal.direct_comparisons << build(:direct_comparisons)
