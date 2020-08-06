81bd3b5c12b8b67c63344acf632346486c914380
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-08-06 17:52:31 +0700

    Change routes for comparisons

--- app/helpers/comparison_helper.rb
+++ app/helpers/comparison_helper.rb
@@ -6,23 +6,23 @@ module ComparisonHelper
     class_option = options[:class]
     case comparison_method
     when 'MagiqComparison'
-      link_to 'Rank compare', new_criterion_magiq_comparison_path(criterion_id, member_id: member_id), class: class_option
+      link_to 'Rank compare', criterion_new_magiq_comparisons_path(criterion_id, member_id: member_id), class: class_option
     when 'DirectComparison'
-      link_to 'Direct compare', new_criterion_direct_comparison_path(criterion_id, member_id: member_id), class: class_option
+      link_to 'Direct compare', criterion_new_direct_comparisons_path(criterion_id, member_id: member_id), class: class_option
     when 'PairwiseComparison'
-      link_to 'Pairwise compare', new_criterion_pairwise_comparison_path(criterion_id, member_id: member_id), class: class_option
+      link_to 'Pairwise compare', criterion_new_pairwise_comparisons_path(criterion_id, member_id: member_id), class: class_option
     end
   end
 
-  def link_to_edit_comparison_method(comparison, options={}, readonly=false)
+  def link_to_edit_comparison_method(comparison, criterion_id, member_id,options={}, readonly=false)
     class_option = options[:class]
     case comparison.type
     when 'MagiqComparison'
-      link_to 'Rank compare', edit_criterion_magiq_comparison_path(criterion_id, comparison.id), class: class_option
+      link_to 'Rank compare', criterion_edit_magiq_comparisonspath(criterion_id, member_id: member_id), class: class_option
     when 'DirectComparison'
-      link_to 'Direct compare', edit_criterion_direct_comparison_path(criterion_id, comparison.id), class: class_option
+      link_to 'Direct compare', criterion_edit_direct_comparisons_path(criterion_id, member_id: member_id), class: class_option
     when 'PairwiseComparison'
-      link_to 'Pairwise compare', edit_criterion_pairwise_comparison_path(criterion_id, comparison.id), class: class_option
+      link_to 'Pairwise compare', criterion_edit_pairwise_comparison_path(criterion_id, member_id: member_id), class: class_option
     end
   end
 
