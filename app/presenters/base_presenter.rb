c0f71584e190951008d4bc04154482b153d64cdf
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-08-07 14:06:45 +0700

    BasePresenter class

---
+++ app/presenters/base_presenter.rb
@@ -0,0 +1,30 @@
+class BasePresenter
+  attr_reader :current_user
+  include Rails.application.routes.url_helpers
+
+  class << self
+    def new(*args)
+      return NilPresenter.new if args[0].nil?
+      super *args
+    end
+
+    def as_collection(collection, method=:as_json, *args)
+      collection.map{|object| self.new(object, *args).send(method) }
+    end
+  end
+
+  def initialize(presentable, curr_user=nil)
+    @presentable = presentable
+    @current_user = curr_user
+  end
+
+  def method_missing(method, *args)
+    @presentable.public_send(method, *args)
+  end
+
+  class NilPresenter
+    def method_missing(method, *args)
+      nil
+    end
+  end
+end
