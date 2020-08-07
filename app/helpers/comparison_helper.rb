e2165696258b95514bf039189c2053c0179d5cdf
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-08-07 15:27:02 +0700

    Change helper for editing comparisons

--- app/helpers/comparison_helper.rb
+++ app/helpers/comparison_helper.rb
@@ -2,28 +2,28 @@ module ComparisonHelper
   include ApplicationHelper
 
   # For Toolbar in Criteria and Ratings
-  def link_to_new_comparison_method(comparison_method, criterion_id, member_id, options={}, readonly=false)
+  def link_to_comparison_method(comparison_method, appraisal, options={}, readonly=false)
+    criterion_id = appraisal.criterion_id
+    member_id = appraisal.member_id
     class_option = options[:class]
-    case comparison_method
-    when 'MagiqComparison'
-      link_to 'Rank compare', criterion_new_magiq_comparisons_path(criterion_id, member_id: member_id), class: class_option
-    when 'DirectComparison'
-      link_to 'Direct compare', criterion_new_direct_comparisons_path(criterion_id, member_id: member_id), class: class_option
-    when 'PairwiseComparison'
-      link_to 'Pairwise compare', criterion_new_pairwise_comparisons_path(criterion_id, member_id: member_id), class: class_option
+    if appraisal.persisted?
+      case comparison_method
+      when 'MagiqComparison'
+        link_to 'Rank compare', criterion_edit_magiq_comparisons_path(criterion_id, member_id: member_id), class: class_option
+      when 'DirectComparison'
+        link_to 'Direct compare', criterion_edit_direct_comparisons_path(criterion_id, member_id: member_id), class: class_option
+      when 'PairwiseComparison'
+        link_to 'Pairwise compare', criterion_edit_pairwise_comparisons_path(criterion_id, member_id: member_id), class: class_option
+      end
+    else
+      case comparison_method
+      when 'MagiqComparison'
+        link_to 'Rank compare', criterion_new_magiq_comparisons_path(criterion_id, member_id: member_id), class: class_option
+      when 'DirectComparison'
+        link_to 'Direct compare', criterion_new_direct_comparisons_path(criterion_id, member_id: member_id), class: class_option
+      when 'PairwiseComparison'
+        link_to 'Pairwise compare', criterion_new_pairwise_comparisons_path(criterion_id, member_id: member_id), class: class_option
+      end
     end
   end
-
-  def link_to_edit_comparison_method(comparison, criterion_id, member_id,options={}, readonly=false)
-    class_option = options[:class]
-    case comparison.type
-    when 'MagiqComparison'
-      link_to 'Rank compare', criterion_edit_magiq_comparisonspath(criterion_id, member_id: member_id), class: class_option
-    when 'DirectComparison'
-      link_to 'Direct compare', criterion_edit_direct_comparisons_path(criterion_id, member_id: member_id), class: class_option
-    when 'PairwiseComparison'
-      link_to 'Pairwise compare', criterion_edit_pairwise_comparison_path(criterion_id, member_id: member_id), class: class_option
-    end
-  end
-
 end
