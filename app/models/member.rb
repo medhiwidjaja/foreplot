3d5db56d856d500a36ce11a5927fc2e37103071b
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-08-11 15:31:33 +0700

    Member validations

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,4 +4,19 @@ class Member < ApplicationRecord
 
   scope :with_user_id, -> (user_id) { where(user_id: user_id) }
   scope :author, -> { where(role: 'owner') }
+
+  validates :user_id, uniqueness: {scope: :article_id}
+  
+  # There should be only 1 owner
+  validates :role, uniqueness: {scope: :article_id}, if: -> { role == 'owner' }
+
+  before_destroy :check_if_owner
+
+  private
+
+  def check_if_owner
+    errors.add(:base, "Cannot delete owner member") if role == 'owner'
+    throw(:abort) if errors.present?
+  end
+
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -2,21 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Member, type: :model do
 
+  let!(:bingley) { create :bingley }
+  let!(:another_user) { create :user } 
+  let!(:article) { create :article, user: bingley }
+  let(:author) { article.members.take }
+  let!(:editor) { create :member, article: article, user:(create :user), role: 'editor' }
+
   describe "scope: author" do
-    let!(:user) { create :user }
-    let!(:article) { create :article, user: user }
-    let!(:author) { article.members.create(user: user, role: 'owner') }
-    let!(:editor) { article.members.create(user: (create :user), role: 'editor') }
-    let!(:viewer) { article.members.create(user: (create :user), role: 'viewer') }
-
     it "includes only the author" do
-      expect(Member.author).to include(author)
+      expect(article.members.author.size).to eq(1)
+      expect(article.members.author.take.user).to eq(bingley)
+    end
+  end
+  
+  describe "validations" do 
+    it "should not allow same user as members" do
+      duplicate = article.members.new(user: bingley, role: 'editor')
+      expect(duplicate).to be_invalid
     end
 
-    it "doesn't include the others" do
-      expect(Member.author).not_to include([editor, viewer])
+    it "should not allow deletion of owner" do
+      owner = article.members.author.take
+      expect{ owner.destroy }.to change(Member, :count).by(0)
     end
-   end
+
+    it "should allow deletion of editor, though" do
+      expect{ editor.destroy }.to change(Member, :count).by(-1)
+    end
+
+    it "should not have more than 1 owner" do
+      thief = build :member, article: article, user: another_user, role: 'owner'
+      expect(thief).to be_invalid
+    end
+  end
 
   describe "associations" do
     it { expect(described_class.reflect_on_association(:user).macro).to eq(:belongs_to) }
