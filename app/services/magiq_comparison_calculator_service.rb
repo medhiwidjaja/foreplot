9b315a6ffccf319d627e914dc380265ad195c0ce
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-08-13 16:01:02 +0700

    Add score_n in magiq comparisons so we can display ratio bar

--- app/services/magiq_comparison_calculator_service.rb
+++ app/services/magiq_comparison_calculator_service.rb
@@ -5,11 +5,18 @@ class MagiqComparisonCalculatorService < BaseCalculatorService
   end
 
   def call
-    @result = convert_to_attributes Foreplot::MagiqRanking.new(params(comparisons)).rank
+    @result = convert_to_attributes(
+      add_score_n_field(
+        Foreplot::MagiqRanking.new(params(comparisons)).rank
+      ))
   end
 
   private 
 
+  def add_score_n_field(hash)
+    hash.map {|x| x.merge({score_n:x[:score]}) }
+  end
+  
   def params(comparisons)
     comparisons.map {|id,param| {id:id, rank:param["rank"].to_i}}
   end
