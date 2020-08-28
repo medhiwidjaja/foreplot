0925ddc8c6259091722653c93e1dc7024dd0e7e5
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-08-28 12:19:24 +0700

    Clean up view specs

--- spec/views/criteria/new.html.slim_spec.rb
+++ spec/views/criteria/new.html.slim_spec.rb
@@ -3,17 +3,19 @@ require 'rails_helper'
 RSpec.describe "criteria/new", type: :view do
 
   let(:article) { FactoryBot.create :article }
-  let(:criterion) { FactoryBot.build :criterion }
+  let(:criterion) { article.criteria.first }
   before(:each) do
     allow(view).to receive(:user_signed_in?) { true } 
     allow(view).to receive(:current_user) { FactoryBot.build(:user) }
     @article = article
     @criterion = criterion
+    @presenter = double(article: article, root: criterion, member_id:1, allow_navigate: true, parent_id: criterion.id, parent: criterion,
+      criterion: criterion, table: nil, comparison_type: 'DirectComparison', appraisal: nil)
   end
 
   it "renders new criterion form" do
     render
-    assert_select "form[action=?][method=?]", criterions_path(@criterion), "post" do
+    assert_select "form[action=?][method=?]", criterion_path(@criterion), "post" do
       assert_select "input[name=?]", "criterion[title]"
       assert_select "input[name=?]", "criterion[abbrev]"
       assert_select "textarea[name=?]", "criterion[description]"
--- spec/views/criteria/show.html.slim_spec.rb
+++ spec/views/criteria/show.html.slim_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "criteria/show", type: :view do
   let(:article) { FactoryBot.create :article }
-  let(:criterion) { FactoryBot.create :criterion }
+  let(:criterion) { article.criteria.first }
   before(:each) do
     allow(view).to receive(:user_signed_in?) { true } 
     allow(view).to receive(:current_user) { FactoryBot.build(:user) }
     @article = article
     @criterion = criterion
+    @presenter = double(article: article, root: criterion, member_id:1, allow_navigate: true, parent_id: criterion.id, parent: criterion,
+      criterion: criterion, table: nil, comparison_type: 'DirectComparison', appraisal: nil)
   end
 
   it "renders attributes" do
