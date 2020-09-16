37f5f55008390270f58306b86e46507fac809650
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-09-16 12:22:21 +0700

    Results #index json

---
+++ app/views/results/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @value_tree_presenter.score_table.values, partial: "results/score", as: :score
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,3 +94,7 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+def body_as_json
+  JSON.parse(response.body, symbolize_names: true)
+end
