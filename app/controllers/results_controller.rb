c7150c28a618da2da3bc0495e85637a054f052b1
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-09-16 12:22:07 +0700

    ResultsController #index

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,22 +1,16 @@
 class ResultsController < ApplicationController
   include TurbolinksCacheControl
-
-  before_action :set_article
-  before_action :set_presenter
+  include ResultsConcern
 
   def index
     respond_to do |format|
       format.html 
+      format.json
     end
   end
 
-  private
-
-  def set_article
-    @article = Article.find(params[:article_id])
+  def chart 
   end
 
-  def set_presenter
-    @presenter = ResultsPresenter.new @article, current_user, {member_id: params[:member_id], article_id: params[:article_id]}
-  end
+
 end
