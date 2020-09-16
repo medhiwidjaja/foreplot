16be042f36efbe630996749c516bf3a91974f0e7
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-09-16 17:33:16 +0700

    ResultsContoller now no longer use a concern to set variables

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,11 @@
 class ResultsController < ApplicationController
   include TurbolinksCacheControl
-  include ResultsConcern
+  
+  before_action :set_article
+  before_action :set_criterion
+  before_action :set_member
+  before_action :set_criterion_presenter, only: :index
+  before_action :set_value_tree_presenter, only: [:index, :chart]
 
   def index
     respond_to do |format|
@@ -15,5 +20,27 @@ class ResultsController < ApplicationController
     end
   end
 
+  private
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
+
+  def set_criterion
+    @criterion = params[:criterion_id] ? Criterion.find(params[:criterion_id]) : @article.criteria.root
+  end
+
+  def set_member
+    @member = params[:member_id] ? Member.find(params[:member_id]) : @article.members.author.take
+  end
+
+  def set_criterion_presenter
+    @criteria_presenter = CriterionPresenter.new @criterion, current_user, {member_id: params[:member_id]}
+  end
+
+  def set_value_tree_presenter
+    @value_tree = ValueTree.new @article.id, @member.id
+    @value_tree_presenter = ValueTreePresenter.new @value_tree, @criterion.id
+  end
 
 end
