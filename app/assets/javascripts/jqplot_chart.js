4f162cde9abd6332aaf9e36e9e3419ad78a7f5ce
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-09-17 11:56:34 +0700

    JS script for chart should only run in results index only

--- app/assets/javascripts/jqplot_chart.js
+++ app/assets/javascripts/jqplot_chart.js
@@ -1,61 +1,63 @@
 $(document).on("ready turbolinks:load", function() {
   var $chart = $('#chart');
-  $.jqplot.config.enablePlugins = true;
-  
-  $.getJSON( '/articles/'+$chart.data("id")+"/results/chart.json?p="+$chart.data('pid'),
-  function(data){
-    var chartData = data.chart_data;
-    var chartLabels = data.labels;
-    var names = data.names;
-    $.jqplot('chart', [chartData], {
-        seriesDefaults:{
-            renderer:$.jqplot.BarRenderer,
-            rendererOptions: {
-                varyBarColor: false
-            },
-            pointLabels: {show: true}
-        },
-        series:[{ pointLabels:{ 
-            show: true, 
-            labels: chartLabels, 
-            edgeTolerance:-25, 
-            escapeHTML: false
-        }}],
-        axes: {
-            xaxis: {
-                renderer: $.jqplot.CategoryAxisRenderer,
-                show: false,
-                showGridline: false, edgeTolerance:-25, escapeHTML: false,
-                tickOptions: { showGridline: false }
-            },
-            yaxis: {
-                padMax: 1.2
-            }
-        },
-        grid: {
-            drawBorder: false,
-            shadow: false
-        },
-        highlighter: {
-            show: true,
-            sizeAdjust: 7.5,
-            tooltipLocation: 'ne',
-            tooltipContentEditor: function(str, seriesIndex, pointIndex) {
-                var objLabel = names[pointIndex];
-                var val = chartData[pointIndex];
-                return "<span style='font-weight:bold; font-size:12pt; color:#333'>"+val.toPrecision(2)+"</span>";
-            }
-        },
-        cursor: {
-            show: false
+  var create_chart = function() { 
+    $.getJSON( '/articles/'+$chart.data("id")+"/results/chart.json?p="+$chart.data('pid'),
+    function(data){
+      var chartData = data.chart_data;
+      var chartLabels = data.labels;
+      var names = data.names;
+      $.jqplot('chart', [chartData], {
+          seriesDefaults:{
+              renderer:$.jqplot.BarRenderer,
+              rendererOptions: {
+                  varyBarColor: false
+              },
+              pointLabels: {show: true}
+          },
+          series:[{ pointLabels:{ 
+              show: true, 
+              labels: chartLabels, 
+              edgeTolerance:-25, 
+              escapeHTML: false
+          }}],
+          axes: {
+              xaxis: {
+                  renderer: $.jqplot.CategoryAxisRenderer,
+                  show: false,
+                  showGridline: false, edgeTolerance:-25, escapeHTML: false,
+                  tickOptions: { showGridline: false }
+              },
+              yaxis: {
+                  padMax: 1.2
+              }
+          },
+          grid: {
+              drawBorder: false,
+              shadow: false
+          },
+          highlighter: {
+              show: true,
+              sizeAdjust: 7.5,
+              tooltipLocation: 'ne',
+              tooltipContentEditor: function(str, seriesIndex, pointIndex) {
+                  var objLabel = names[pointIndex];
+                  var val = chartData[pointIndex];
+                  return "<span style='font-weight:bold; font-size:12pt; color:#333'>"+val.toPrecision(2)+"</span>";
+              }
+          },
+          cursor: {
+              show: false
+          }
+      });
+      $('#chart').bind('jqplotDataClick', 
+        function (ev, seriesIndex, pointIndex, data) {
+            $('#point-info').html('series: '+ names[pointIndex]+', value: '+data[1].toPrecision(3));
         }
+      );
     });
-    $('#chart').bind('jqplotDataClick', 
-      function (ev, seriesIndex, pointIndex, data) {
-          $('#point-info').html('series: '+ names[pointIndex]+', value: '+data[1].toPrecision(3));
-      }
-    );
-  });
-
-  
+  };
+  if ($chart.length > 0) { 
+    $.jqplot.config.enablePlugins = true;
+    create_chart();
+  };
 });
