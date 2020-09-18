5b82c9db8e1dd44b9e73a2196dd282cb3b63889a
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-09-18 16:21:42 +0700

    AHP Comparison with polymorphic order

--- app/models/ahp_comparison.rb
+++ app/models/ahp_comparison.rb
@@ -1,3 +1,30 @@
-
 class AHPComparison < Comparison
+
+  Need to handle joins with polymorphic relation
+  scope :order_by_position, -> {
+    joins(<<-SQL.squish
+      LEFT OUTER JOIN criteria c
+      ON c.id = comparisons.comparable_id 
+      AND comparisons.comparable_type = 'Criterion'
+
+      LEFT OUTER JOIN alternatives a
+      ON a.id = comparisons.comparable_id 
+      AND comparisons.comparable_type = 'Alternative'
+    SQL
+    )
+    .select(<<-SQL.squish
+       comparisons.*,
+       CASE WHEN a.id IS NULL THEN COALESCE(c.position, c.id)
+            WHEN c.id IS NULL THEN COALESCE(a.position, a.id)
+            ELSE NULL
+       END AS position,
+       CASE WHEN a.id IS NULL THEN COALESCE(c.title, '')
+            WHEN c.id IS NULL THEN COALESCE(a.title, '')
+            ELSE NULL
+       END AS name
+    SQL
+    )
+    .order('position ASC')
+  }
+
 end
