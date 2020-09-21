556c22550fc85f2b4315d67e17a9693561a59dd9
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-09-21 12:50:26 +0700

    DRY up specs for AHP Comparison ordering by position

--- spec/support/shared_contexts/shared_comparison_context.rb
+++ spec/support/shared_contexts/shared_comparison_context.rb
@@ -5,12 +5,12 @@ RSpec.shared_context "criteria context for comparisons", :shared_context => :met
   let(:root)      { article.criteria.first }
   let(:criterion) { root }
   
-  let(:c1)   { create :criterion, article: article, parent: criterion, position: 1 }
-  let(:c2)   { create :criterion, article: article, parent: criterion, position: 2 }
-  let(:c3)   { create :criterion, article: article, parent: criterion, position: 3 }
+  let(:c1)   { create :criterion, article: article, parent: criterion }
+  let(:c2)   { create :criterion, article: article, parent: criterion }
+  let(:c3)   { create :criterion, article: article, parent: criterion }
 
-  let(:alt1) { create :alternative, article: article, position: 1 }
-  let(:alt2) { create :alternative, article: article, position: 2 }
-  let(:alt3) { create :alternative, article: article, position: 3 }
+  let(:alt1) { create :alternative, article: article }
+  let(:alt2) { create :alternative, article: article }
+  let(:alt3) { create :alternative, article: article }
 
 end
