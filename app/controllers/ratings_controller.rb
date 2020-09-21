a40361e81271e247a8c80722d39a6109e1a6795a
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-09-21 09:36:47 +0700

    Ratings #index and #show

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,23 +1,19 @@
 class RatingsController < ApplicationController
   include TurbolinksCacheControl
 
-  before_action :set_criterion
-  before_action :set_presenter
-
   def index
+    @article = Article.find params[:article_id]
+    @criterion = @article.criteria.root 
+    @presenter = CriterionPresenter.new @criterion, current_user, {member_id: params[:member_id], article_id: params[:article_id]}
+  end
+
+  def show
+    @criterion = Criterion.includes(:appraisals).find(params[:criterion_id])
+    @presenter = CriterionPresenter.new @criterion, current_user, {member_id: params[:member_id], article_id: params[:article_id]}
     respond_to do |format|
       format.html 
       format.js 
     end
   end
 
-  private
-
-  def set_criterion
-    @criterion = Criterion.includes(:appraisals).find(params[:criterion_id])
-  end
-
-  def set_presenter
-    @presenter = CriterionPresenter.new @criterion, current_user, {member_id: params[:member_id], article_id: params[:article_id]}
-  end
 end
