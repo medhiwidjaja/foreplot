80d8bf9d0d7780654a7350be771b7e15f417a84d
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-09-21 16:32:50 +0700

    AHP comparisons results return comparable titles

--- spec/requests/ahp_comparison_request_spec.rb
+++ spec/requests/ahp_comparison_request_spec.rb
@@ -113,6 +113,11 @@ RSpec.describe "AHPComparisons", type: :request do
         expect(response.status).to eql 302
         expect(response).to redirect_to(criterion)
         follow_redirect!
+        expect(response).to render_template(:show)
+        expect(response.body).to include('AHP comparisons updated')
+        # expect(response.body).to include(c1.title)
+        # expect(response.body).to include(c2.title)
+        # expect(response.body).to include(c3.title)
       end
     end
 
@@ -122,6 +127,10 @@ RSpec.describe "AHPComparisons", type: :request do
         expect(response.status).to eql 302
         expect(response).to redirect_to(criterion)
         follow_redirect!
+        expect(response).to render_template(:show)
+        expect(response.body).to include(c1.title)
+        expect(response.body).to include(c2.title)
+        expect(response.body).to include(c3.title)
       end
     end
   end
@@ -153,6 +162,10 @@ RSpec.describe "AHPComparisons", type: :request do
         expect(response.status).to eql 302
         expect(response).to redirect_to(criterion_ratings_path(c1))
         follow_redirect!
+        expect(response).to render_template(:show)
+        expect(response.body).to include(alt1.title)
+        expect(response.body).to include(alt2.title)
+        expect(response.body).to include(alt3.title)
       end
     end
 
@@ -162,6 +175,11 @@ RSpec.describe "AHPComparisons", type: :request do
         expect(response.status).to eql 302
         expect(response).to redirect_to(criterion_ratings_path(c1))
         follow_redirect!
+        expect(response).to render_template(:show)
+        expect(response.body).to include('AHP comparisons updated')
+        expect(response.body).to include(alt1.title)
+        expect(response.body).to include(alt2.title)
+        expect(response.body).to include(alt3.title)
       end
     end
   
