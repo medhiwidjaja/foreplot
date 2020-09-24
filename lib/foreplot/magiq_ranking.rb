9aa4bcedaeb317fedbd28ace807c2c708351cf97
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-09-24 17:33:03 +0700

    MagiqRanking delegates calculation to OrdinalScore

--- lib/foreplot/magiq_ranking.rb
+++ lib/foreplot/magiq_ranking.rb
@@ -6,26 +6,23 @@ module Foreplot
   #  [{id:, rank:, score:}]
 
   class MagiqRanking 
-    include Foreplot::Magiq::OrdinalScore
 
     attr_reader :results
 
-    def initialize(choices)
-      @choices = choices.to_a.clone.map(&:clone)  # Deep cloning within the array
+    def initialize(choices, rank_method = :rank_order_centroid)
+      @choices = choices.to_a.clone.map(&:clone).sort_by {|c| c[:rank] }  # Deep cloned, sorted array
+      @rank_method = rank_method
       @results = []
     end
 
-    # Score the choice based on it's rank i, within k number of choices
-    # Use the supplied rank_method (:rank_order_centroid, :rank_sum, :rank_reciprocal, :rank_exponential)
-    #  use :rank_order_centroid if none supplied
-    def rank(rank_method = :rank_order_centroid)
-      k = @choices.size
-      @results = @choices
-        .sort_by {|c| c[:rank] }
-        .collect {|c| 
-          c.update( {score: send(rank_method, k, c[:rank])} )
-        }
+    def rank
+      scores = Foreplot::Magiq::OrdinalScore.score( choices.map{|c| c[:rank] }, rank_method )
+      @results = choices.map.with_index { |c, i| c.update( {score: scores[i]} ) }
     end
-  end
 
+    private
+
+    attr_reader :choices, :rank_method
+
+  end
 end
--- spec/lib/foreplot/magiq_ranking_spec.rb
+++ spec/lib/foreplot/magiq_ranking_spec.rb
@@ -2,15 +2,15 @@ require 'foreplot/magiq/ordinal_score'
 require 'foreplot/magiq_ranking'
 
 RSpec.describe Foreplot::MagiqRanking do
-  let(:choices) { [{id:1, name:'Apple',rank:3}, {id:2, name:'IBM',rank:2}, {id:3, name:'Commodore',rank:1}, {id:4, name:'Atari', rank:5}] }
+  let(:choices) { [{id:1, name:'Apple',rank:3}, {id:2, name:'IBM',rank:2}, {id:3, name:'Commodore',rank:1}, {id:4, name:'Atari', rank:4}] }
   let(:expected_result) { 
-    [{:id=>3, :name=>"Commodore", :rank=>1, :score=>0.5208333333333334}, {:id=>2, :name=>"IBM", :rank=>2, :score=>0.2708333333333333}, {:id=>1, :name=>"Apple", :rank=>3, :score=>0.14583333333333331}, {:id=>4, :name=>"Atari", :rank=>5, :score=>0}]
+    [{:id=>3, :name=>"Commodore", :rank=>1, :score=>0.5208333333333334}, {:id=>2, :name=>"IBM", :rank=>2, :score=>0.2708333333333333}, {:id=>1, :name=>"Apple", :rank=>3, :score=>0.14583333333333331}, {:id=>4, :name=>"Atari", :rank=>4, :score=>0.0625}]
   }
   let(:rank_exponential_result) {
-    [{:id=>3, :name=>"Commodore", :rank=>1, :score=>0.27991631056633925}, {:id=>2, :name=>"IBM", :rank=>2, :score=>0.26426549301342617}, {:id=>1, :name=>"Apple", :rank=>3, :score=>0.2436813018392995}, {:id=>4, :name=>"Atari", :rank=>5, :score=>0.0}]
+    [{:id=>3, :name=>"Commodore", :rank=>1, :score=>0.27991631056633925}, {:id=>2, :name=>"IBM", :rank=>2, :score=>0.26426549301342617}, {:id=>1, :name=>"Apple", :rank=>3, :score=>0.2436813018392995}, {:id=>4, :name=>"Atari", :rank=>4, :score=>0.21213689458093501}]
   }
   let(:rank_sum_result){
-    [{:id=>3, :name=>"Commodore", :rank=>1, :score=>0.4}, {:id=>2, :name=>"IBM", :rank=>2, :score=>0.3}, {:id=>1, :name=>"Apple", :rank=>3, :score=>0.2}, {:id=>4, :name=>"Atari", :rank=>5, :score=>0.0}]
+    [{:id=>3, :name=>"Commodore", :rank=>1, :score=>0.4}, {:id=>2, :name=>"IBM", :rank=>2, :score=>0.3}, {:id=>1, :name=>"Apple", :rank=>3, :score=>0.2}, {:id=>4, :name=>"Atari", :rank=>4, :score=>0.1}]
   }
   
   describe ".new" do
@@ -32,8 +32,8 @@ RSpec.describe Foreplot::MagiqRanking do
 
   describe ".rank with rank_sum function" do
     it "calculates the results" do
-      magiq_ranking = Foreplot::MagiqRanking.new choices
-      results = magiq_ranking.rank :rank_sum
+      magiq_ranking = Foreplot::MagiqRanking.new choices, :rank_sum
+      results = magiq_ranking.rank
       expect(results).to eq(rank_sum_result)
       expect(magiq_ranking.results).to eq(rank_sum_result)
     end
@@ -41,8 +41,8 @@ RSpec.describe Foreplot::MagiqRanking do
 
   describe ".rank with rank_exponential function" do
     it "calculates the results" do
-      magiq_ranking = Foreplot::MagiqRanking.new choices
-      results = magiq_ranking.rank :rank_exponential
+      magiq_ranking = Foreplot::MagiqRanking.new choices, :rank_exponential
+      results = magiq_ranking.rank
       expect(results).to eq(rank_exponential_result)
       expect(magiq_ranking.results).to eq(rank_exponential_result)
     end
