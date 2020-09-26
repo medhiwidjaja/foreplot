0f224b047f713e8b2e493add5581821f828ddcc4
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-09-26 17:34:27 +0700

    Direct comparison calculator with options

--- app/services/direct_comparison_calculator.rb
+++ app/services/direct_comparison_calculator.rb
@@ -1,11 +1,12 @@
 class DirectComparisonCalculator < BaseCalculator
   attr_reader :comparisons, :result
-  def initialize(comparisons)
+  def initialize(comparisons, **options)
     @comparisons = comparisons
+    @options = options
   end
 
   def call
-    @result = convert_to_attributes Foreplot::DirectRanking.new(params(comparisons)).rank
+    @result = convert_to_attributes Foreplot::DirectRanking.new(params(comparisons), @options).rank
   end
 
   private 
