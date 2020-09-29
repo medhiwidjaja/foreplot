a69f20b77d7a3c2a78d582e21e55c7fbf88d4d34
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-09-29 16:05:07 +0700

    Results nav menu improved

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,8 +1,8 @@
 module ResultsHelper
   include ApplicationHelper
 
-  def link_or_text(is_label, path, text)
-    return content_tag(:li, content_tag(:a, text), class: 'active') if is_label
+  def link_or_text(label, path, text)
+    return content_tag(:li, content_tag(:a, text), class: 'active') if controller_name == label
     
     content_tag :li, link_to(text, path)
   end
