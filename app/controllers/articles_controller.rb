deac2f934b60105e644852882706e265cb5ccc37
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-10-01 17:40:21 +0700

    Article form: updated for remote form validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,29 +32,20 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /articles/1
   # PATCH/PUT /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.' 
+    else
+      render :edit
     end
   end
 
---
+++ spec/features/article_feature_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+RSpec.feature "Article", type: :feature do
+  context "Logged in user" do
+    let!(:bingley) { create :bingley }
+    let(:valid_attributes) {
+      { title: 'Article 1', description: 'Article number one', user_id: bingley.id }
+    }
+    before(:each) { login_as bingley, scope: :user }
+
+    scenario "User creates a new article" do
+      visit new_article_path
+      fill_in 'article_title',       with: valid_attributes[:title]
+      fill_in 'article_description', with: valid_attributes[:description]
+      click_button 'Save'
+      expect(page).to have_css('h3', text: 'Article 1')
+    end
+
+    before {
+      @article = create :article, user: bingley
+    }
+    scenario "User edits an article" do 
+      visit article_path(@article)
+      click_link 'Edit'
+      fill_in 'article_title',       with: valid_attributes[:title]
+      fill_in 'article_description', with: valid_attributes[:description]
+      click_button 'Save'
+      expect(page).to have_css('h3', text: 'Article 1')
+      expect(page).to have_content 'Article number one'
+    end
+
+    scenario "User doesn't fill in the title field" do
+      visit new_article_path
+      fill_in 'article_title',       with: ''
+      click_button 'Save'
+      expect(page).to have_content "can't be blank"
+    end
+
+  end
+
+end
