13781ab3ba5dc5a65f996876745d6c1f4b17dbae
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-10-15 15:22:14 +0700

    Eager loading /lib folder: solving problem in production

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Desight
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     
-    config.autoload_paths      += %W[#{config.root}/app]
-    config.autoload_once_paths += %W[#{config.root}/lib]
+    config.autoload_paths   << Rails.root.join('app')
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
