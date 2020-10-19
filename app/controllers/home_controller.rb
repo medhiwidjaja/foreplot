43b730287fd11d50ea44a1f14c76948417086d88
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-10-19 09:50:03 +0700

    Eager load articles authors in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index  
-    @featured_articles = Article.public_articles
+    @featured_articles = Article.public_articles.includes(:user)
   end
 end
