7ee0d44ba0649e8a5a3987927c446173476fd318
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-10-19 16:33:28 +0700

    Update mailer credentials for production

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   address: Rails.application.credentials.smtp_address,
-  port: 587,
+  port: Rails.application.credentials.smtp_port,
   domain: Rails.application.credentials.smtp_domain,
   user_name: Rails.application.credentials.smtp_user_name,
   password: Rails.application.credentials.smtp_password,
